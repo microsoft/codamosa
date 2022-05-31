334a808afdf95907d2916e75cb4951554498ba78
Caroline Lemieux <caro.lemieux@gmail.com>
2022-05-31 19:22:14 -0400

    get freevariablevisitor/operator working

--- pynguin/languagemodels/astscoping.py
+++ pynguin/languagemodels/astscoping.py
@@ -10,6 +10,75 @@ from typing import Any, Set, List, Dict, Callable
 
 import pynguin.testcase.variablereference as vr
 
+class FreeVariableOperator(ast.NodeTransformer):
+
+    def __init__(self,  operation: Callable[[ast.expr], Any]):
+        self._bound_variables = set()
+        self._operator = operation
+
+    def visit_Name(self, node: ast.Name) -> Any:
+        if node.id not in self._bound_variables:
+            return self._operator(node)
+        else:
+            return node
+
+    def visit_Call(self, node: ast.Call) -> ast.Call:
+        new_args = [self.visit(arg) for arg in node.args]
+        new_kwargs = [ast.keyword(arg=kwarg.arg, value=self.visit(kwarg.value)) for kwarg in node.keywords]
+        return ast.Call(func=node.func, args=new_args, keywords=new_kwargs)
+
+    def visit_Lambda(self, node: ast.Lambda) -> ast.Lambda:
+        bound_variables_before = set(self._bound_variables)
+        all_args: ast.arguments = node.args
+        for arg in all_args.args + all_args.kwonlyargs:
+            arg_name = arg.arg
+            self._bound_variables.add(arg_name)
+        if all_args.kwarg is not None:
+            self._bound_variables.add(all_args.kwarg.arg)
+        if all_args.vararg is not None:
+            self._bound_variables.add(all_args.vararg.arg)
+        new_body = self.visit(node.body)
+        self._bound_variables = bound_variables_before
+        return ast.Lambda(args=node.args, body=new_body)
+
+    def get_comprehension_bound_vars(self, node: ast.comprehension) -> List[str]:
+        return [elem.id for elem in ast.walk(node.target) if isinstance(elem, ast.Name)]
+
+    def _visit_generators_common(self, generators: List[ast.comprehension]):
+        new_generators = []
+        for comp in generators:
+            self._bound_variables.update(self.get_comprehension_bound_vars(comp))
+            new_generators.append(ast.comprehension(target=comp.target, iter=self.visit(comp.iter),
+                                                    ifs=[self.visit(iff) for iff in comp.ifs],
+                                                    is_async=comp.is_async))
+        return new_generators
+
+    def visit_ListComp(self, node: ast.ListComp) -> ast.ListComp:
+        bound_variables_before = set(self._bound_variables)
+        new_generators = self._visit_generators_common(node.generators)
+        new_elt = self.visit(node.elt)
+        ret_val = ast.ListComp(elt=new_elt, generators=new_generators)
+        self._bound_variables = bound_variables_before
+        return ret_val
+
+    def visit_SetComp(self, node: ast.SetComp) -> ast.SetComp:
+        bound_variables_before = set(self._bound_variables)
+        new_generators = self._visit_generators_common(node.generators)
+        new_elt = self.visit(node.elt)
+        ret_val = ast.SetComp(elt=new_elt, generators=new_generators)
+        self._bound_variables = bound_variables_before
+        return ret_val
+
+    def visit_DictComp(self, node: ast.DictComp) -> ast.DictComp:
+        bound_variables_before = set(self._bound_variables)
+        new_generators = self._visit_generators_common(node.generators)
+        new_key = self.visit(node.key)
+        new_value = self.visit(node.value)
+        ret_val = ast.DictComp(key=new_key, value=new_value, generators=new_generators)
+        self._bound_variables = bound_variables_before
+        return ret_val
+
+
 
 def operate_on_free_variables(node: ast.expr, operation: Callable[[ast.expr], Any]) -> ast.expr:
     """Visits `node` and applies an operation on all the free variables in `node`,
@@ -23,7 +92,8 @@ def operate_on_free_variables(node: ast.expr, operation: Callable[[ast.expr], An
     Returns:
         a, possibly strange, ast.expr
     """
-    return node
+    transformed_node = FreeVariableOperator(operation).visit(node)
+    return transformed_node
 
 
 def _replace_with_var_refs(node: ast.AST, ref_dict):
--- tests/languagemodel/test_astscoping.py
+++ tests/languagemodel/test_astscoping.py
@@ -21,7 +21,7 @@ list_comprehension = '[i + 1 for i in var_0]'
 list_comprehension_res = '[i + 1 for i in MOO]'
 
 nested_list_comprehension = '[obj for objs in var_0 for obj in objs]'
-nested_list_comprehension_res = '[obj for objs in var_0 for obj in objs]'
+nested_list_comprehension_res = '[obj for objs in MOO for obj in objs]'
 
 lambda_in_list_comprehension = '[lambda x: x + i for i in var_0]'
 lambda_in_list_comprehension_res = '[lambda x: x + i for i in MOO]'
@@ -36,6 +36,21 @@ dict_comprehension_res = '{i: i + MOO for i in MOO}'
 fn_call_on_bound = '{foo(i) for i in var_0}'
 fn_call_on_bound_res = '{foo(i) for i in MOO}'
 
+free_var_name_collision = '[x for x in var_0] + x'
+free_var_name_collision_res = '[x for x in MOO] + MOO'
+
+setcomp_name_collision = '{x for x in var_0} + x'
+setcomp_name_collision_res = '{x for x in MOO} + MOO'
+
+dictcomp_name_collision = '{x: x for x in var_0} + x'
+dictcomp_name_collision_res = '{x: x for x in MOO} + MOO'
+
+lambda_name_collision = '(lambda x: x) + x'
+lambda_name_collision_res = '(lambda x: x) + MOO'
+
+# Why would anyone ever do this?
+nested_list_name_collision = '[xs for x in xs for xs in x]'
+nested_list_name_collision_res = '[xs for x in MOO for xs in x]'
 
 @pytest.mark.parametrize(
     "input_str,output_str",
@@ -47,7 +62,12 @@ fn_call_on_bound_res = '{foo(i) for i in MOO}'
         (lambda_in_list_comprehension, lambda_in_list_comprehension_res),
         (set_comprehension, set_comprehension_res),
         (dict_comprehension, dict_comprehension_res),
-        (fn_call_on_bound, fn_call_on_bound_res)
+        (fn_call_on_bound, fn_call_on_bound_res),
+        (free_var_name_collision, free_var_name_collision_res),
+        (setcomp_name_collision, setcomp_name_collision_res),
+        (dictcomp_name_collision, dictcomp_name_collision_res),
+        (lambda_name_collision, lambda_name_collision_res),
+        (nested_list_name_collision, nested_list_name_collision_res)
     ],
 )
 def test_moo_operate(input_str, output_str):
